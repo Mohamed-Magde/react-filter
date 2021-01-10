c2360cde2e179f909006e20f3678746e4bbed225
Mohamed-Magde <askandrani.1996@gmail.com>
2021-01-10 14:06:04 +0300

    Add Filter
    Add new Array for Filtered Elements
    Filter The Elements from The Main List and push them to FilteredList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 import { data } from "./data/data";
 
 function App() {
   const [list, setList] = useState([...data]);
-  useEffect(() => {
-    console.log(list);
-  }, [list]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [desc, setDesc] = useState("");
 
   const addRandom = () => {
     var text = "";
@@ -17,16 +16,42 @@ function App() {
       text += possible.charAt(Math.floor(Math.random() * possible.length));
     setList((prev) => [...prev, { id: prev.length + 2, text: text }]);
   };
+  const handleChange = (e) => {
+    setDesc(e.target.value);
+    let newList = [...list];
+    if (desc !== "") {
+      newList = newList.filter(
+        (item) => item.text.toLowerCase().search(desc.toLowerCase()) !== -1
+      );
+      setFilteredList([...newList]);
+    }
+  };
+
   return (
     <div className="container">
-      <input type="text" placeholder="search" name="" id="" />
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="search"
+        name="desc"
+        id=""
+      />
       <button onClick={addRandom}>Add Text</button>
-      <div className="text-list">
-        {list.map((item) => {
-          const { text, id } = item;
-          return <p key={id}>{text}</p>;
-        })}
-      </div>
+      {filteredList.length === 0 || desc === "" ? (
+        <div className="text-list">
+          {list.map((item) => {
+            const { text, id } = item;
+            return <p key={id}>{text}</p>;
+          })}
+        </div>
+      ) : (
+        <div className="text-list">
+          {filteredList.map((item) => {
+            const { text, id } = item;
+            return <p key={id}>{text}</p>;
+          })}
+        </div>
+      )}
     </div>
   );
 }
