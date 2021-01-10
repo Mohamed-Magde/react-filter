216784746eab1099bb8be71a454d2b06ae36ccaa
Mohamed-Magde <askandrani.1996@gmail.com>
2021-01-10 14:59:53 +0300

    add SCSS for styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import "./App.css";
-import { data } from "./data/data";
-
+import { data } from "./assets/data/data";
+import "./assets/scss/main.css";
 function App() {
   const [list, setList] = useState([...data]);
   const [filteredList, setFilteredList] = useState([]);
@@ -19,39 +18,54 @@ function App() {
   const handleChange = (e) => {
     setDesc(e.target.value);
     let newList = [...list];
-    if (desc !== "") {
-      newList = newList.filter(
-        (item) => item.text.toLowerCase().search(desc.toLowerCase()) !== -1
-      );
-      setFilteredList([...newList]);
-    }
+
+    newList = newList.filter(
+      (item) =>
+        item.text.toLowerCase().search(e.target.value.toLowerCase()) !== -1
+    );
+
+    setFilteredList([...newList]);
   };
 
   return (
-    <div className="container">
-      <input
-        type="text"
-        onChange={handleChange}
-        placeholder="search"
-        name="desc"
-        id=""
-      />
-      <button onClick={addRandom}>Add Text</button>
-      {filteredList.length === 0 || desc === "" ? (
-        <div className="text-list">
-          {list.map((item) => {
-            const { text, id } = item;
-            return <p key={id}>{text}</p>;
-          })}
+    <div className="main">
+      <div className="container">
+        <h2 className="main-header">My Filter App</h2>
+        <div className="main-search">
+          <input
+            type="text"
+            onChange={handleChange}
+            placeholder="search"
+            name="desc"
+            id=""
+          />
+          <button onClick={addRandom}>
+            <i className="fas fa-plus"></i>
+          </button>
         </div>
-      ) : (
-        <div className="text-list">
-          {filteredList.map((item) => {
-            const { text, id } = item;
-            return <p key={id}>{text}</p>;
-          })}
-        </div>
-      )}
+        {desc === "" ? (
+          <div className="text-list">
+            {list.map((item) => {
+              const { text, id } = item;
+              return <p key={id}>{text}</p>;
+            })}
+          </div>
+        ) : (
+          <div className="text-list">
+            {filteredList.length === 0 ? (
+              <h3 className="danger">Element Not Found</h3>
+            ) : (
+              <>
+                {" "}
+                {filteredList.map((item) => {
+                  const { text, id } = item;
+                  return <p key={id}>{text}</p>;
+                })}
+              </>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
